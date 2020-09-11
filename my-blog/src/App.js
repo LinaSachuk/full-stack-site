08d2b8b38bdf02dfefb3b29633a9bb32c76e7d93
Lina Sachuk <sachuklina@gmail.com>
2020-09-10 23:21:19 -0600

    add pages

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,6 +4,11 @@ import {
   Route
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+
+
 import './App.css';
 
 class App extends Component {
@@ -11,7 +16,14 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Route path="/" component={HomePage} exact />
+          <div id="page-body">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/articles-list" component={ArticlesList} />
+            <Route path="/article" component={ArticlePage} />
+
+          </div>
+
         </div>
 
       </Router>
