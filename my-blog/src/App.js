0296da401805fa987de174e8d3c3c355ee0b086b
Lina Sachuk <sachuklina@gmail.com>
2020-09-11 23:38:56 -0600

    add Not Found Page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import NotFoundPage from './pages/NotFoundPage';
 import Navbar from './NavBar';
 import './App.css';
 
@@ -17,10 +19,13 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <div id="page-body">
-            <Route path="/" component={HomePage} exact />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/articles-list" component={ArticlesList} />
-            <Route path="/article/:name" component={ArticlePage} />
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/articles-list" component={ArticlesListPage} />
+              <Route path="/article/:name" component={ArticlePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
 
           </div>
 
