6a2c804fe7b38e4c8222d2f3912ee631cd1df31f
Lina Sachuk <sachuklina@gmail.com>
2020-09-15 23:12:01 -0600

    add comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -5,12 +5,15 @@ import bodyParser from 'body-parser';
 const articlesInfo = {
     'learn-react': {
         upvotes: 0,
+        comments: [],
     },
     'learn-node': {
         upvotes: 0,
+        comments: [],
     },
     'my-thoughts-on-resumes': {
         upvotes: 0,
+        comments: [],
     }
 }
 
@@ -23,7 +26,7 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
 
     articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`$${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+    res.status(200).send(`$${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`)
 })
 
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`))
