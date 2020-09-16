426bcde294931c2b4eed4142047134e042aed7ba
Lina Sachuk <sachuklina@gmail.com>
2020-09-15 23:02:16 -0600

    add upvotes

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -19,7 +19,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const articleName = req.params.name;
 
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`$${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+})
 
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`))
 
