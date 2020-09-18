a5a464109d471dbf55a0a27ec18b1d8d46d97686
Lina Sachuk <sachuklina@gmail.com>
2020-09-17 23:28:49 -0600

    add mongodb

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -26,8 +26,22 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
 
     articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`$${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`)
+
+    res.status(200).send(`$${articleName} 
+    now has ${articlesInfo[articleName].upvotes} upvotes!`)
+});
+
+app.post('/api/articles/:name/add-comment', (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({ username, text });
+
+    res.status(200).send(articlesInfo[articleName]);
 })
+
+
+
 
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`))
 
