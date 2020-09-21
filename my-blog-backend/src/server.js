18f5c5799c2bcac0e75bab4540b502aa7678bdbc
Lina Sachuk <sachuklina@gmail.com>
2020-09-20 23:39:02 -0600

    add query

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -12,16 +12,23 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/api/articles/:name', (req, res) => {
-    const articleName = req.params.name;
+app.get('/api/articles/:name', async (req, res) => {
 
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-    const db = client.db('my-blog');
+    try {
+        const articleName = req.params.name;
 
-    const articleInfo = await db.collection('articles').findOne({ name: articleName })
-    res.status(200).json(articleInfo);
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
 
-    client.close();
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+
+
 
 
 })
@@ -29,10 +36,11 @@ app.get('/api/articles/:name', (req, res) => {
 app.post('/api/articles/:name/upvote', (req, res) => {
     const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes += 1;
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('my-blog');
 
-    res.status(200).send(`$${articleName} 
-    now has ${articlesInfo[articleName].upvotes} upvotes!`)
+
+
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
